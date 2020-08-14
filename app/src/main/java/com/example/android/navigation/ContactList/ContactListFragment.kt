f36b0aa423e7faaa8211e8aa6386da8a2121f0c0
TNyKk <kingkuka088@gmail.com>
2020-08-14 18:24:37 +0700

    add navigation fragment_contact_list.xml

--- app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
@@ -1,4 +1,4 @@
-package ContactList
+package com.example.android.navigation.ContactList
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,19 +7,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.android.navigation.R
-import com.example.android.navigation.databinding.FragmentContactListBinding
 
 class ContactListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        // Get a reference to the binding object and inflate the fragment views.
-        val binding: FragmentContactListBinding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_contact_list, container, false)
-
-        val application = requireNotNull(this.activity).application
-
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_list, container, false)
     }
 }
