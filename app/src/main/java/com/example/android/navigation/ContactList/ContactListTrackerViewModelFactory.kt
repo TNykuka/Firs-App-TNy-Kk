c14b2cef85336f9d547edc291cfe0b7aee15c6b4
TNyKk <kingkuka088@gmail.com>
2020-08-15 00:26:05 +0700

    Add ContactListTrackerViewModelFactory.kt

--- app/src/main/java/com/example/android/navigation/ContactList/ContactListTrackerViewModelFactory.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactListTrackerViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.example.android.navigation.ContactList
 
-class ContactListTrackerViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.navigation.database.ListDatabaseDao
+
+class ContactListTrackerViewModelFactory(
+        private val dataSource: ListDatabaseDao,
+        private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ContactTrackerViewModel::class.java)) {
+            return ContactTrackerViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
