7ca39be1ea81249f09796db82e2988298b9e39d4
TNyKk <kingkuka088@gmail.com>
2020-08-15 01:59:58 +0700

    add Util.kt

--- app/src/main/java/com/example/android/navigation/ContactList/ContactTrackerViewModel.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactTrackerViewModel.kt
@@ -3,9 +3,17 @@ package com.example.android.navigation.ContactList
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.android.navigation.database.ListDatabaseDao
+import kotlinx.coroutines.Job
 
 class ContactTrackerViewModel(
         val database: ListDatabaseDao,
                               application: Application) : AndroidViewModel(application) {
 
+
+    private var viewModelJob = Job()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
 }
