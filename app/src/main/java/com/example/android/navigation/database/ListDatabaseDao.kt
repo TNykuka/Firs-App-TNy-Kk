10498b230555c7dac81ea817c7e874d2583a29d1
TNyKk <kingkuka088@gmail.com>
2020-08-14 23:02:15 +0700

    create File ListDatabase.kt ListDatabaseDao.kt ListNight.kt

--- app/src/main/java/com/example/android/navigation/database/ListDatabaseDao.kt
+++ app/src/main/java/com/example/android/navigation/database/ListDatabaseDao.kt
@@ -1,4 +1,29 @@
 package com.example.android.navigation.database
 
-class ListDatabaseDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface ListDatabaseDao{
+
+    @Insert
+    fun insert(night: ListNight)
+
+    @Update
+    fun update(night: ListNight)
+
+    @Query("SELECT * from daily_list_quality_table WHERE namelistId= :key")
+    fun get(key: Long): ListNight?
+
+    @Query("DELETE FROM daily_list_quality_table")
+    fun clear()
+
+    @Query("SELECT * FROM daily_list_quality_table ORDER BY namelistId DESC LIMIT 1")
+    fun getTonight(): ListNight?
+
+    @Query("SELECT * FROM daily_list_quality_table ORDER BY namelistId DESC")
+    fun getAllNights(): LiveData<List<ListNight>>
 }
--- app/src/main/java/com/example/android/navigation/database/ListNight.kt
+++ app/src/main/java/com/example/android/navigation/database/ListNight.kt
@@ -1,4 +1,14 @@
 package com.example.android.navigation.database
 
-class ListNight {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_list_quality_table")
+data class ListNight(
+        @PrimaryKey(autoGenerate = true)
+        var namelistId: Long = 0L,
+
+        @ColumnInfo(name = "name_list_Quality")
+        var namelistQuality: String = ""
+)
