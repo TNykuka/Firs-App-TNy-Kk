990bdbe254c001bc73febbdab0ebb0695b42fb69
TNyKk <kingkuka088@gmail.com>
2020-08-15 00:53:34 +0700

    Val dataSource

--- app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
@@ -7,11 +7,16 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.android.navigation.R
+import com.example.android.navigation.database.ListDatabase
 
 class ContactListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = ListDatabase.getInstance(application).ListDatabaseDao
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact_list, container, false)
     }
