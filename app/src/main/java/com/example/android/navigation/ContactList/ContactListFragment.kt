6748210b1bf4f6bbbb3fb65c04f89324e20b3df5
TNyKk <kingkuka088@gmail.com>
2020-08-15 01:03:38 +0700

    val binding: FragmentContactListBinding

--- app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactListFragment.kt
@@ -6,19 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.android.navigation.R
 import com.example.android.navigation.database.ListDatabase
+import com.example.android.navigation.databinding.FragmentContactListBinding
 
 class ContactListFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
+        val binding: FragmentContactListBinding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_contact_list, container, false)
+
         val application = requireNotNull(this.activity).application
         val dataSource = ListDatabase.getInstance(application).ListDatabaseDao
         val viewModelFactory = ContactListTrackerViewModelFactory(dataSource, application)
 
+        val sleepTrackerViewModel =
+                ViewModelProvider(
+                        this, viewModelFactory).get(ContactTrackerViewModel::class.java)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_list, container, false)
+
+
+        return binding.root
     }
 }
