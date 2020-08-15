3ed7a133204a48c99c7384188a6d60ed9e581340
TNyKk <kingkuka088@gmail.com>
2020-08-15 17:47:55 +0700

    edit ListDatabase.kt

--- app/src/main/java/com/example/android/navigation/database/ListDatabase.kt
+++ app/src/main/java/com/example/android/navigation/database/ListDatabase.kt
@@ -6,20 +6,20 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [ListNight::class],version = 1)
-abstract class SubscriberDatabase : RoomDatabase() {
+abstract class ListDatabase : RoomDatabase() {
 
-    abstract val subscriberDAO : ListDatabaseDao
+    abstract val listDatabaseDao : ListDatabaseDao
 
     companion object{
         @Volatile
-        private var INSTANCE : SubscriberDatabase? = null
-        fun getInstance(context: Context):SubscriberDatabase{
+        private var INSTANCE : ListDatabase? = null
+        fun getInstance(context: Context):ListDatabase{
             synchronized(this){
                 var instance = INSTANCE
                 if(instance==null){
                     instance = Room.databaseBuilder(
                             context.applicationContext,
-                            SubscriberDatabase::class.java,
+                            ListDatabase::class.java,
                             "subscriber_data_database"
                     ).build()
                 }
