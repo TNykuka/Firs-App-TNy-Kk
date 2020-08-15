b61559ce9d464ec977be70301e4b498b5c819389
TNyKk <kingkuka088@gmail.com>
2020-08-16 02:37:25 +0700

    Implement UserViewModel.kt

--- app/src/main/java/com/example/android/navigation/Adduser/UserViewModel.kt
+++ app/src/main/java/com/example/android/navigation/Adduser/UserViewModel.kt
@@ -1,4 +1,26 @@
 package com.example.android.navigation.Adduser
 
-class UserViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: UserRepository
+
+    val allWords: LiveData<List<Usar>>
+
+    init {
+        val usersDao = UserRoomDatabase.getDatabase(application).wordDao()
+        repository = UserRepository(usersDao)
+        allWords = repository.allWords
+    }
+
+
+    fun insert(user : Usar) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(user)
+    }
 }
