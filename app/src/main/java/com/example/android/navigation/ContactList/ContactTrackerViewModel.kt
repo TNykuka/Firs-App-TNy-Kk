2742a8fa669456ea73c4863c9316202b6dcab762
TNyKk <kingkuka088@gmail.com>
2020-08-15 22:43:01 +0700

    edit ContactTrackerViewModel.kt Implement the ViewModel

--- app/src/main/java/com/example/android/navigation/ContactList/ContactTrackerViewModel.kt
+++ app/src/main/java/com/example/android/navigation/ContactList/ContactTrackerViewModel.kt
@@ -1,56 +1,31 @@
 package com.example.android.navigation.ContactList
 
 import android.app.Application
-import android.provider.SyncStateContract.Helpers.insert
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.android.navigation.database.ListDatabase
 import com.example.android.navigation.database.ListDatabaseDao
 import com.example.android.navigation.database.ListNight
+import com.example.android.navigation.database.ListRepository
 import kotlinx.coroutines.*
 
-class ContactTrackerViewModel(
-        val database: ListDatabaseDao,
-                              application: Application) : AndroidViewModel(application) {
+class ContactTrackerViewModel(application: Application) : AndroidViewModel(application) {
 
 
-    private var viewModelJob = Job()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-    private var tonight = MutableLiveData<ListNight?>()
+    private val repository: ListRepository
 
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
-    }
+    val allWords: LiveData<List<ListNight>>
 
     init {
-        initializeTonight()
+        val wordsDao = ListDatabase.getDatabase(application).listDatabaseDao()
+        repository = ListRepository(wordsDao)
+        allWords = repository.allWords
     }
 
-    private fun initializeTonight() {
-        uiScope.launch {
-            tonight.value = getTonightFromDatabase()
-        }
-    }
-
-    private suspend fun getTonightFromDatabase(): ListNight? {
-        return withContext(Dispatchers.IO) {
-            var night = database.getTonight()
-
-            night
-        }
-    }
-
-    fun onStartTracking(){
-        uiScope.launch {
-            val newNight = ListNight()
-            insert(newNight)
-        }
-    }
-
-    private suspend fun insert(newNight: ListNight) {
-        withContext(Dispatchers.IO) {
-            database.insert(newNight)
-        }
+    fun insert(listNight: ListNight) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(listNight)
     }
 
 }
