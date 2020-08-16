dedd33e3f6384aa05a01689ec2307a35c7059d27
TNyKk <kingkuka088@gmail.com>
2020-08-16 12:36:50 +0700

    Add NewUserActivity

--- app/src/main/java/com/example/android/navigation/Adduser/UserRoomDatabase.kt
+++ app/src/main/java/com/example/android/navigation/Adduser/UserRoomDatabase.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = arrayOf(User::class), version = 1, exportSchema = false)
 abstract class UserRoomDatabase : RoomDatabase() {
@@ -19,18 +21,35 @@ abstract class UserRoomDatabase : RoomDatabase() {
                 context: Context,
                 scope: CoroutineScope
         ): UserRoomDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                         context.applicationContext,
                         UserRoomDatabase::class.java,
                         "word_database"
-                ).build()
+                )
+                        .fallbackToDestructiveMigration()
+                        .addCallback(UserDatabaseCallback(scope))
+                        .build()
                 INSTANCE = instance
-                return instance
+                instance
+            }
+        }
+
+        private class UserDatabaseCallback(
+                private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                INSTANCE?.let { database ->
+                    scope.launch {
+                        populateDatabase(database.userDao())
+                    }
+                }
+            }
+
+            fun populateDatabase(userDao: UserDao) {
+                userDao.deleteAll()
             }
         }
     }
