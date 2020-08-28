20fc6d9e4742ca1f61efbf3e89d037c854070195
TNyKk <kingkuka088@gmail.com>
2020-08-28 19:21:18 +0700

    Annotate the Contact.kt data class

--- app/src/main/java/com/example/android/navigation/Contactdatabase/Contact.kt
+++ app/src/main/java/com/example/android/navigation/Contactdatabase/Contact.kt
@@ -1,7 +1,15 @@
 package com.example.android.navigation.Contactdatabase
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact_table")
 data class Contact (
+        @PrimaryKey(autoGenerate = true)
         var id: Long = 0L,
+        @ColumnInfo(name = "name")
         var name: String = "unnamed",
+        @ColumnInfo(name = "phone")
         var phone: String = ""
 )
