a6af78b7cc20bd2651789a18b8e64a64ae377054
TNyKk <kingkuka088@gmail.com>
2020-08-28 19:29:43 +0700

    Create the Database class

--- app/src/main/java/com/example/android/navigation/ContactFragment.kt
+++ app/src/main/java/com/example/android/navigation/ContactFragment.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.navigation.Contactdatabase
+package com.example.android.navigation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
